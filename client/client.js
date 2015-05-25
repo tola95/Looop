092f5411fe79a6bbb88843c8340870db8252d351
Deanna  Hartsook <deanna.hartsook14@imperial.ac.uk>
2015-05-25 16:38:31 +0100

    fixed issue causing same note to always be played

--- client/client.js
+++ client/client.js
@@ -2,19 +2,18 @@ Template.buttons.helpers({
   /* Piano notes by http://www.freesound.org/people/jobro/
      Attribution license: http://creativecommons.org/licenses/by/3.0/ */
   audio_file1: "/sounds/piano/piano-32.wav",
-  audio_file2: "sounds/piano/piano-33.wav",
-  audio_file3: "sounds/piano/piano-34.wav",
-  audio_file4: "sounds/piano/piano-35.wav",
-  audio_file5: "sounds/piano/piano-36.wav",
-  audio_file6: "sounds/piano/piano-37.wav",
-  audio_file7: "sounds/piano/piano-38.wav",
-  audio_file8: "sounds/piano/piano-39.wav",
-  audio_file9: "sounds/piano/piano-40.wav",
+  audio_file2: "/sounds/piano/piano-33.wav",
+  audio_file3: "/sounds/piano/piano-34.wav",
+  audio_file4: "/sounds/piano/piano-35.wav",
+  audio_file5: "/sounds/piano/piano-36.wav",
+  audio_file6: "/sounds/piano/piano-37.wav",
+  audio_file7: "/sounds/piano/piano-38.wav",
+  audio_file8: "/sounds/piano/piano-39.wav",
+  audio_file9: "/sounds/piano/piano-40.wav",
 });
 
-Template.buttons.events({
-  'click button': function (e, template) {
+Template.button.events({
+  'click': function (e, template) {
     // Play corresponding audio file
-    template.find('audio').play();
-  }
+    template.find('audio').play();  }
 }); 
