24c080cb2bc83e2cd71ed9eb7e8bc8fd0b180c9b
Deanna  Hartsook <deanna.hartsook14@imperial.ac.uk>
2015-05-28 14:22:14 +0100

    Added sounds to the keyboard and made them change according to instrument

--- client/client.js
+++ client/client.js
@@ -1,26 +1,20 @@
-/* Piano notes by http://www.freesound.org/people/jobro/
-   Attribution license: http://creativecommons.org/licenses/by/3.0/ */
-Session.setDefault("audio_file1", "/sounds/piano/piano-A4.wav");
-Session.setDefault("audio_file2", "/sounds/piano/piano-Bb4.wav");
-Session.setDefault("audio_file3", "/sounds/piano/piano-B4.wav");
-Session.setDefault("audio_file4", "/sounds/piano/piano-C4.wav");
-Session.setDefault("audio_file5", "/sounds/piano/piano-Db4.wav");
-Session.setDefault("audio_file6", "/sounds/piano/piano-D4.wav");
-Session.setDefault("audio_file7", "/sounds/piano/piano-Eb4.wav");
-Session.setDefault("audio_file8", "/sounds/piano/piano-E4.wav");
-Session.setDefault("audio_file9", "/sounds/piano/piano-F4.wav");
+var STARTING_DRUM = "drum1";
+var STARTING_KEYBOARD = "grandpiano";
 
-getSoundPath = function(sound) {
-  return "/sounds/piano/piano-" + sound + ".wav";
+// Set the default drum and keyboard notes
+window.onload = function() {
+  updateDrumSounds(getInstrumentSounds(STARTING_DRUM));
+  updatePianoSounds(getInstrumentSounds(STARTING_KEYBOARD));
 }
 
+// Retrieves the array of paths for the given instrument from the database
 getInstrumentSounds = function(instrument) {
   return Sounds.findOne({instrument: instrument}).paths;
 }  
 
 
 populate = function() {
-  var instruments = ["drum1", "drum2", "grandPiano", "churchOrgan"];
+  var instruments = ["drum1", "drum2", "grandpiano", "churchorgan"];
   var instrumentSounds = {
     "drum1": ["piano/piano-C4", "piano/piano-Db4", "piano/piano-D4", "piano/piano-Eb4", "piano/piano-E4", 
       "piano/piano-F4", "piano/piano-Gb4", "piano/piano-G4", "piano/piano-Ab4"],
@@ -28,13 +22,13 @@ populate = function() {
     "drum2": ["piano/piano-C3", "piano/piano-Db3", "piano/piano-D3", "piano/piano-Eb3", "piano/piano-E3", 
       "piano/piano-F3", "piano/piano-Gb3", "piano/piano-G3", "piano/piano-Ab3"],
 
-    "grandPiano": ["piano/piano-C4", "piano/piano-Db4", "piano/piano-D4", "piano/piano-Eb4", "piano/piano-E4", 
+    "grandpiano": ["piano/piano-C4", "piano/piano-Db4", "piano/piano-D4", "piano/piano-Eb4", "piano/piano-E4", 
       "piano/piano-F4", "piano/piano-Gb4", "piano/piano-G4", "piano/piano-Ab4", "piano/piano-A5", 
       "piano/piano-Bb5", "piano/piano-B5", "piano/piano-C5", "piano/piano-Db5", "piano/piano-D5", 
       "piano/piano-Eb5", "piano/piano-E5", "piano/piano-F5", "piano/piano-Gb5", "piano/piano-G5", 
       "piano/piano-Ab5", "piano/piano-A6", "piano/piano-Bb6", "piano/piano-B6"],
 
-    "churchOrgan": ["piano/piano-C4", "piano/piano-Db4", "piano/piano-D4", "piano/piano-Eb4", "piano/piano-E4", 
+    "churchorgan": ["piano/piano-C4", "piano/piano-Db4", "piano/piano-D4", "piano/piano-Eb4", "piano/piano-E4", 
       "piano/piano-F4", "piano/piano-Gb4", "piano/piano-G4", "piano/piano-Ab4", "piano/piano-A5", 
       "piano/piano-Bb5", "piano/piano-B5", "piano/piano-C5", "piano/piano-Db5", "piano/piano-D5", 
       "piano/piano-Eb5", "piano/piano-E5", "piano/piano-F5", "piano/piano-Gb5", "piano/piano-G5", 
@@ -54,7 +48,7 @@ populate = function() {
 };
 
 
-Template.buttons.helpers({
+Template.drum_buttons.helpers({
   audio_file1: function () { return Session.get("audio_file1"); },
   audio_file2: function () { return Session.get("audio_file2"); },
   audio_file3: function () { return Session.get("audio_file3"); },
@@ -66,21 +60,9 @@ Template.buttons.helpers({
   audio_file9: function () { return Session.get("audio_file9"); },
 });
 
-
-Template.body.events({
-  'click #choice_submit': function() {
-    var button_dropdown = document.getElementById("button_choice");
-    var button_num = button_dropdown.options[button_dropdown.selectedIndex].value;
-    var sound_dropdown = document.getElementById("sound_choice");
-    var sound = sound_dropdown.options[sound_dropdown.selectedIndex].value;
-    Session.set("audio_file" + button_num, getSoundPath(sound));
-  }
-});
-
 Template.soundpad_button.events({
   'click': function (e, template) {
     // Play corresponding audio file
-    var sounds = getInstrumentSounds("grandPiano");
     template.find('audio').play();
   }
 });
@@ -111,7 +93,7 @@ Accounts.ui.config({
 });
 
 
-
+// Respond to events in the instrument menu
 Template.menu.events = {
   'click #drum1': function() {
     document.getElementById("buttoncontainer").style.display = "block";
@@ -151,13 +133,54 @@ Template.menu.events = {
       updateDrumSounds(sounds);
     }
   }
-}
+};
 
 // Updates the buttons to play the tracks with the given paths
 updateDrumSounds = function(paths) {
   for (var i = 0; i<paths.length; i++) {
     Session.set("audio_file" + (i+1), paths[i]);
   }
+};
 
-}
+// Updates the keys to play the tracks with the given paths
+updatePianoSounds = function(paths) {
+  for (var i = 0; i<paths.length; i++) {
+    Session.set("key" + (i+1), paths[i]);
+  }
+};
+
+// Variables for each audio file path needed for the keyboard
+Template.keys.helpers({
+  key1: function() { return Session.get("key1"); },
+  key2: function() { return Session.get("key2"); },
+  key3: function() { return Session.get("key3"); },
+  key4: function() { return Session.get("key4"); },
+  key5: function() { return Session.get("key5"); },
+  key6: function() { return Session.get("key6"); },
+  key7: function() { return Session.get("key7"); },
+  key8: function() { return Session.get("key8"); },
+  key9: function() { return Session.get("key9"); },
+  key10: function() { return Session.get("key10"); },
+  key11: function() { return Session.get("key11"); },
+  key12: function() { return Session.get("key12"); },
+  key13: function() { return Session.get("key13"); },
+  key14: function() { return Session.get("key14"); },
+  key15: function() { return Session.get("key15"); },
+  key16: function() { return Session.get("key16"); },
+  key17: function() { return Session.get("key17"); },
+  key18: function() { return Session.get("key18"); },
+  key19: function() { return Session.get("key19"); },
+  key20: function() { return Session.get("key20"); },
+  key21: function() { return Session.get("key21"); },
+  key22: function() { return Session.get("key22"); },
+  key23: function() { return Session.get("key23"); },
+  key24: function() { return Session.get("key24"); }
+});
+
+Template.keys.events({
+  'click .playable': function(e, template) {
+    var audio = e.target.getElementsByClassName("audio")[0];
+    audio.play();
+  }
+});
 
