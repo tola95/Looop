690daf2792260e12f1e508223e62fad20fa8ae0a
Tola Babasola <OmotolaBabasola1@pc-158-133.wireless.lyra.downing.lan>
2015-06-03 21:58:45 +0100

    Added user file. Contains stuff relating to user pages. Not much in it currently but just wanted to have it on git.

---
+++ User/user.js
@@ -0,0 +1,9 @@
+function showRecordings() {
+   document.getElementById('recordings').style.display = "block";
+   document.getElementById('timeline').style.display = "none";
+}
+
+function showTimeline() {
+   document.getElementById('recordings').style.display = "none";
+   document.getElementById('timeline').style.display = "block";
+}
